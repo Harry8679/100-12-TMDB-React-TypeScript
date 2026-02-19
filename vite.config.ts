cc711006fe3f1664cda749f0f6ba82a233eac4a5
Harry7986 <emarh.harry.code@gmail.com>
2026-02-19 12:48:59 +0100

    Vite Config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
+  plugins: [react()],
+  base: '/100-12-TMDB-React-TypeScript/', // ⚠️ Avec le slash à la fin
 })
