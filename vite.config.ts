16cbff6816af5375d23eacd21fd55af125b910e2
Harry7986 <emarh.harry.code@gmail.com>
2026-02-19 13:15:38 +0100

    index css

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/100-12-TMDB-React-TypeScript/', // ⚠️ Avec le slash à la fin
+  base: '/100-12-TMDB-React-TypeScript/',
 })
